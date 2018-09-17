48330b6176f3eaaf42607ea4a980b15386853cc4
Devin Petersohn <devin.petersohn@gmail.com>
2018-09-16 21:11:57 -0700

    Removing dead code

--- modin/pandas/test/test_dataframe.py
+++ modin/pandas/test/test_dataframe.py
@@ -3568,7 +3568,5 @@ def test_get_dummies():
     frame_data = {'A': ['a', 'b', 'a'], 'B': ['b', 'a', 'c'], 'C': [1, 2, 3]}
     ray_df = pd.DataFrame(frame_data)
     pd_df = pandas.DataFrame(frame_data)
-    print(pd.get_dummies(ray_df))
-    print(pandas.get_dummies(pd_df))
     assert ray_df_equals_pandas(
         pd.get_dummies(ray_df), pandas.get_dummies(pd_df))
